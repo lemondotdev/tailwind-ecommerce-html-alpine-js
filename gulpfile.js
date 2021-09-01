ebe423d0bbf5b242708252c52c500c6bdae99eed
Daniel Neshkoski <daniel@digitalnode.com>
2021-09-01 12:31:01 +0200

    gulp concat for js files

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const {src, dest} = require('gulp');
+var concat = require('gulp-concat');
+
+const allScripts = () => 
+    src('./src/js/*js')
+    .pipe(concat('alljs.js'))
+    .pipe(dest('build/js'));
+
+    exports.allScripts = allScripts;
